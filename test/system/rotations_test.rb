9bc60f3f383290e062f08d9326436903171be2b2
Tyler Chamberland <41761895+tylerchamberland@users.noreply.github.com>
2019-03-28 00:14:50 -0400

    generate system/integration test

---
+++ test/system/rotations_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class RotationsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit rotations_url
+  #
+  #   assert_selector "h1", text: "Rotations"
+  # end
+end
